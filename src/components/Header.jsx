01ad16d1a098f08297597424995f4a95ea5ce14d
sergei00787 <sergei007_87@mail.ru>
2020-02-28 23:20:18 +0700

    Initial project #14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header className={cssHeader.header}>
       <img src="https://banner2.cleanpng.com/20180422/aqw/kisspng-logo-lapel-pin-mexico-3d-design-affixed-5adcdf6be17623.1829017415244245559235.jpg" />
+      
     </header>
   )
 }
