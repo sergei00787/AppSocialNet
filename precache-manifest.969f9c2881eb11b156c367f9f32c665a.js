e410bf2f6a9ff064eb82976377170b653d0f87be
sergei00787 <sergei007_87@mail.ru>
2020-05-18 20:04:54 +0700

    Updates

---
+++ precache-manifest.969f9c2881eb11b156c367f9f32c665a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a0b1f4307ab12e5548dec64ba626fd",
+    "url": "/sergei00787/AppSocialNet/index.html"
+  },
+  {
+    "revision": "48f478cbd722a04e948d",
+    "url": "/sergei00787/AppSocialNet/static/css/3.9a21b80b.chunk.css"
+  },
+  {
+    "revision": "fd0e54b8800f2ad4476a",
+    "url": "/sergei00787/AppSocialNet/static/css/4.f2df9e95.chunk.css"
+  },
+  {
+    "revision": "ad4cbd92cb6f8d2c0f34",
+    "url": "/sergei00787/AppSocialNet/static/css/5.32414bd6.chunk.css"
+  },
+  {
+    "revision": "f3d8fb2c3be079ad8ffc",
+    "url": "/sergei00787/AppSocialNet/static/css/main.3041e931.chunk.css"
+  },
+  {
+    "revision": "3fff8c80337622141a02",
+    "url": "/sergei00787/AppSocialNet/static/js/2.36f2f10f.chunk.js"
+  },
+  {
+    "revision": "76bbb5dbcf32f9e8b66fee98c30112db",
+    "url": "/sergei00787/AppSocialNet/static/js/2.36f2f10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f478cbd722a04e948d",
+    "url": "/sergei00787/AppSocialNet/static/js/3.3a80f1c2.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/sergei00787/AppSocialNet/static/js/3.3a80f1c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0e54b8800f2ad4476a",
+    "url": "/sergei00787/AppSocialNet/static/js/4.cc0ed95e.chunk.js"
+  },
+  {
+    "revision": "ad4cbd92cb6f8d2c0f34",
+    "url": "/sergei00787/AppSocialNet/static/js/5.85625f63.chunk.js"
+  },
+  {
+    "revision": "f3d8fb2c3be079ad8ffc",
+    "url": "/sergei00787/AppSocialNet/static/js/main.e59e2b9a.chunk.js"
+  },
+  {
+    "revision": "8e82713cc1e5ab9f15ae",
+    "url": "/sergei00787/AppSocialNet/static/js/runtime-main.83bf56ec.js"
+  },
+  {
+    "revision": "ccaf3211f4a5d70609f68545bcb7d091",
+    "url": "/sergei00787/AppSocialNet/static/media/preloader.ccaf3211.svg"
+  }
+]);
